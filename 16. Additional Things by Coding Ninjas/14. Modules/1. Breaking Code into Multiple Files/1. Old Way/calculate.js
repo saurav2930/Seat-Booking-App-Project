9c53922436292ea55d826c09776ffe87f4fc06a9
Saurav Kumar <saurav20001@gmail.com>
2026-02-28 22:50:39 +0530

    14.Modules folder removed

--- 16. Additional Things by Coding Ninjas/14. Modules/1. Breaking Code into Multiple Files/1. Old Way/calculate.js
+++
@@ -1,7 +0,0 @@
-// calculate.js
-
-const PI = Math.PI
-function calculateArea(radius) {
-    return PI * radius * radius;
-  }
-  
--- 16. Additional Things by Coding Ninjas/14. Modules/1. Breaking Code into Multiple Files/2. ES6 Module Way/calculate.js
+++
@@ -1,5 +0,0 @@
-// calculation.js
-export function calculateArea(radius) {
-    return Math.PI * radius * radius;
-  }
-  
